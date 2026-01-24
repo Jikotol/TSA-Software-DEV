36f26d0651a58ffdd34847ca8d02293c6ff45b6a
Jikotol <maafsmlrao@gmail.com>
2026-01-24 02:51:34 +0000

    Continued tutorial into sqlalchemy

--- learnFlask/tutorial.py
+++ learnFlask/tutorial.py
@@ -1,9 +1,25 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(hours=1) # starts counting when session is created apparaently
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100)) # num in string the max num of chrs
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 
 @app.route("/")
 def home():
@@ -12,30 +28,58 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login(): # send info using post request
     if request.method == "POST":
+        session.permanent = True # makes it expire w/ the time thing
         user = request.form["nm"] 
         session["user"] = user
+        print(users)
+        found_user = users.query.filter_by(name=user).first()  # how to grab info from db, returns a users obj
+        if found_user:
+            session["email"] = found_user.email
+        else: # user doesn't exist, so gotta make obj
+            usr = users(user, "")
+            db.session.add(usr) # kinda like staging area
+            db.session.commit() # ALWAYS commit when make change to database
+
+
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already logged in!")
             return redirect(url_for("user"))
         
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+
+        if request.method == "POST": # email form was submitted
+            email = request.form["email"] # email is the name in html
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was saved!")
+        else:
+            if "email" in session: # user alr entered in email and now getting from session to display
+                email = session["email"]
+        return render_template("user.html", email=email)
     else:
+        flash("You are not logged in")
         return redirect(url_for("login"))
     
 @app.route("/logout")
 def logout():
+    flash(f"You have been logged out!", "info")
     session.pop("user", None)
-    flash("You have been logged out!", "info")
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 # !!!!!!! Test out closing web browser clears session data! cant tset rn
