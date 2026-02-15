56326f2fa2fd315ac0060b86f20ed4b0bdeedffb
Jikotol <maafsmlrao@gmail.com>
2026-02-14 18:42:41 -0800

    Refactored study mode, wrote docstrings and comments for app.py, started on vocab-list

--- static/sets.js
+++ static/sets.js
@@ -4,48 +4,46 @@ Handles event handling and calling the functions
 
 import { loadCard, flipCard, nextCard, previousCard } from "./flashcard.js"
 
-const setId = window.location.pathname.split('/').pop();
-const cardContainer = document.getElementById("card-div");
-// Fetches set object and list of card objects
-const request = await fetch("/api/study/" + setId);
-const data = await request.json();
+export async function initCardData(setId, cardContainer) {
 
-const cards = data.cards;
-const setName = data.set_name;
+    // Fetches set object and list of card objects
+    const request = await fetch("/api/study/" + setId);
+    const data = await request.json();
 
+    const cards = data.cards;
+    const setName = data.set_name;
 
-console.log(cards);
+    // Holds changing values
+    let state = {
+        index: 0,
+        side: "front",
+        termEl: null,
+        visualEl: null,
+        cardContainer: cardContainer
+    }
 
-console.log("ehllo, worked")
-console.log(document)
-
-// Holds changing values
-let state = {
-    index: 0,
-    side: "front",
-    termEl: null,
-    visualEl: null,
-    cardContainer: cardContainer
+    return [cards, state]
 }
 
-// Flips card
-cardContainer.addEventListener("click", () => {flipCard(cards, state)});
+export function setupFlashcardActions(cards, cardContainer, document, state) {
+    /* Dynamically creates the event handlers with given data and html */
 
-// Goes to the next flashcard
-document.getElementById("next-btn").addEventListener("click", () => {
-        nextCard(cards, document, state);
-    });
+    // Flips card
+    cardContainer.addEventListener("click", () => {flipCard(cards, state)});
 
-// Goes to the previous flashcard
-document
-    .getElementById("back-btn")
-    .addEventListener("click", () => {previousCard(cards, document, state)});
-
-document.addEventListener("DOMContentLoaded", () => {
-    loadCard(cards, document, state);
+    // Goes to the next flashcard
+    document.getElementById("next-btn").addEventListener("click", () => {
+            nextCard(cards, document, state);
+        });
+                
+    // Goes to the previous flashcard
     document
-        .getElementById("name-header")
-        .innerText = setName;
-})
-
-loadCard(cards, document, state);
+        .getElementById("back-btn")
+        .addEventListener("click", () => {previousCard(cards, document, state)});
+                
+    document.addEventListener("DOMContentLoaded", () => {
+        loadCard(cards, document, state);
+        })
+    
+    loadCard(cards, document, state);
+}
